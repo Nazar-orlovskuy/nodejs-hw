b56b372443ddb7ac9cacefd1fdcec2e811fbb738
Nazar_O <nazar.orlovskuy49@gmail.com>
2026-01-23 22:55:59 -0600

    Updated notes middleware1

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,16 @@
-export const errorHandler = (err, req, res, next) => {
-  const status = err.status || 500;
+import createHttpError from 'http-errors';
 
-  res.status(status).json({
-    message: err.message || 'Internal Server Error',
-  });
+export const errorHandler = (err, req, res, next) => {
+  let status = 500;
+  let message = 'Internal Server Error';
+
+
+  if (createHttpError.isHttpError(err)) {
+    status = err.status;
+    message = err.message;
+  } else if (err instanceof Error) {
+    message = err.message;
+  }
+
+  res.status(status).json({ message });
 };
