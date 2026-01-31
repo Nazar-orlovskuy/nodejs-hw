6b3aa3e7c8212ca1c184daa27584fc04f91f2f5b
Nazar_O <nazar.orlovskuy49@gmail.com>
2026-01-30 21:54:29 -0600

    validationsecond

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import {
   getAllNotes,
   getNoteById,
@@ -9,10 +10,39 @@ import {
 
 const router = Router();
 
+
+const noteIdSchema = celebrate({
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().hex().length(24).required(),
+  }),
+});
+
+const createNoteSchema = celebrate({
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(3).max(100).required(),
+    content: Joi.string().min(1).required(),
+  }),
+});
+
+const updateNoteSchema = celebrate({
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().hex().length(24).required(),
+  }),
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(3).max(100),
+    content: Joi.string().min(1),
+  }),
+});
+
+
 router.get('/notes', getAllNotes);
-router.get('/notes/:noteId', getNoteById);
-router.post('/notes', createNote);
-router.delete('/notes/:noteId', deleteNote);
-router.patch('/notes/:noteId', updateNote);
+
+router.get('/notes/:noteId', noteIdSchema, getNoteById);
+
+router.post('/notes', createNoteSchema, createNote);
+
+router.delete('/notes/:noteId', noteIdSchema, deleteNote);
+
+router.patch('/notes/:noteId', updateNoteSchema, updateNote);
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -10,6 +10,9 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
+
+import { errors as celebrateErrors } from 'celebrate';
+
 const app = express();
 
 await connectMongoDB();
@@ -19,6 +22,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(notesRoutes);
+
+app.use(celebrateErrors());
 
 app.use(notFoundHandler);
 app.use(errorHandler);
